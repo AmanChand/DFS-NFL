06de7ee459e061977dca5ffe037eb9c06a360faf
AmanChand <amanchand45@gmail.com>
2016-06-19 22:17:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " @a1_alcoholic. All rights reserved.</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Target.html'>Who To Target</a></li>" +
+"<li><a href='Avoid.html'>Who To Avoid</a></li>" +
+"<li><a href='Lineups.html'>Lineups</a></li>" +
+"<li><a href='Results.html'>Lineup Results</a></li>" +
+"<li><a href='about.html'>About Me</a></li>" +
+"<li><a href='contact.html'>Contact Me</a></li>" +
+"</ul>"; 
+
+
